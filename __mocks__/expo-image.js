10f8fc334014f5242b33ef5548ad7d701f09eaa9
Tylor-V <vandenbrosales@gmail.com>
2025-08-27 12:02:47 -0700

    Update expo-image.js

--- __mocks__/expo-image.js
+++ __mocks__/expo-image.js
@@ -2,6 +2,9 @@ module.exports = {
   Image: jest.fn().mockImplementation(({children, ...props}) => {
     return require('react').createElement('Image', props, children);
   }),
+  ImageBackground: jest.fn().mockImplementation(({children, ...props}) => {
+    return require('react').createElement('ImageBackground', props, children);
+  }),
   // default export
   default: jest.fn().mockImplementation(({children, ...props}) => {
     return require('react').createElement('Image', props, children);
