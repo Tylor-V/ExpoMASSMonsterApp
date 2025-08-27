12d3c953f716205a51aaffea5c0dccf11b58d98f
Tylor-V <vandenbrosales@gmail.com>
2025-08-26 21:27:09 -0700

    Update SwipeableTabs.tsx

--- components/SwipeableTabs.tsx
+++ components/SwipeableTabs.tsx
@@ -148,7 +148,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: colors.background,
+    backgroundColor: colors.black,
     shadowColor: '#000',
     shadowOpacity: 0.3,
     shadowOffset: { width: 0, height: -2 },
