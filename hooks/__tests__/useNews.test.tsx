77fa14645bf5e0a0d48772b143c00197a314069a
Tylor-V <vandenbrosales@gmail.com>
2025-08-27 16:03:12 -0700

    Create useNews.test.tsx

---
+++ hooks/__tests__/useNews.test.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react-native';
+import React from 'react';
+import { Text } from 'react-native';
+import { useNews } from '../useNews';
+
+jest.mock('../../firebase/firebase', () => ({
+  firestore: () => ({
+    collection: () => ({
+      where: () => ({
+        orderBy: () => ({
+          onSnapshot: (cb: any) => {
+            cb({
+              docs: [{ id: '1', data: () => ({ message: 'Test news' }) }],
+            });
+            return () => {};
+          },
+        }),
+      }),
+    }),
+  }),
+}));
+
+describe('useNews', () => {
+  it('loads news from firestore', () => {
+    const Comp = () => {
+      const { news, loading } = useNews();
+      return <Text>{loading ? 'loading' : news[0]?.message}</Text>;
+    };
+    const { getByText } = render(<Comp />);
+    getByText('Test news');
+  });
+});
