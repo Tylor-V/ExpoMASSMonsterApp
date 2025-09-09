d7e7e20e7a08d744972a3590dc5ba974b65bac2c
Tylor-V <vandenbrosales@gmail.com>
2025-09-08 18:41:07 -0700

    Update categoryRatings.test.ts

--- utils/__tests__/categoryRatings.test.ts
+++ utils/__tests__/categoryRatings.test.ts
@@ -1,4 +1,4 @@
-import { parseCategoryRatings } from '../categoryRatings';
+import { parseCategoryRatings, stripCategoryRatings } from '../categoryRatings';
 
 describe('parseCategoryRatings', () => {
   it('extracts ratings from description', () => {
@@ -14,5 +14,17 @@ describe('parseCategoryRatings', () => {
   it('ignores values outside 0-5', () => {
     const desc = 'Strength/Performance: 6 stars';
     expect(parseCategoryRatings(desc)).toEqual({});
+  });
+});
+
+describe('stripCategoryRatings', () => {
+  it('removes rating text from description', () => {
+    const desc =
+      'Energy/Focus: 4 stars General Health: 3 stars Product description';
+    expect(stripCategoryRatings(desc)).toBe('Product description');
+  });
+
+  it('returns empty string when input undefined', () => {
+    expect(stripCategoryRatings(undefined)).toBe('');
   });
 });
