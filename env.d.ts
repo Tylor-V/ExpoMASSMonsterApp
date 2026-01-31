f99043f4d0c0f192743534eec271fdae1d512206
Tylor-V <vandenbrosales@gmail.com>
2026-01-31 14:25:22 -0800

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 module.exports = {
   GOOGLE_PLACES_API_KEY: 'test-key',
   SHOPIFY_DOMAIN: 'test-domain',
-  SHOPIFY_API_VERSION: '2024-01',
+  SHOPIFY_API_VERSION: '2026-01',
   SHOPIFY_TOKEN: 'test-token',
   SHOPIFY_ADMIN_TOKEN: 'test-admin-token',
   AGORA_APP_ID: 'test-id',
